e430e51b1b6033eff984b9d12bb335d9fb78c1f6
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-05-01 01:08:04 +0530

    Cors

--- app/main.py
+++ app/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from app.routers.router import router as main_router
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
 load_dotenv()
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # List of allowed origins
+    allow_credentials=False,           # Allow cookies, authorization headers, etc.
+    allow_methods=["*"],              # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],              # Allow all headers
+)
 
 
 async def not_found(request, exc):
